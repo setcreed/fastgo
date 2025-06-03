b2fba479ced93a6ae56e0a178ab8fb599c26457b
chengwz <cwzdzg@foxmail.com>
2025-06-03 17:21:23 +0800

    feat: 增加viper配置

--- cmd/fg-apiserver/main.go
+++ cmd/fg-apiserver/main.go
@@ -3,6 +3,12 @@ package main
 import (
 	"os"
 
+	// 导入 automaxprocs 包，可以在程序启动时自动设置 GOMAXPROCS 配置，
+	// 使其与 Linux 容器的 CPU 配额相匹配。
+	// 这避免了在容器中运行时，因默认 GOMAXPROCS 值不合适导致的性能问题，
+	// 确保 Go 程序能够充分利用可用的 CPU 资源，避免 CPU 浪费。
+	_ "go.uber.org/automaxprocs"
+
 	"github.com/setcreed/fastgo/cmd/fg-apiserver/app"
 )
 
